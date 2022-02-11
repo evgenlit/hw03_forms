5a53f23e4ded7c3ed6e5bb43ad02443539c68610
Evgeny Lityushkin <tcifrovik@gmail.com>
2022-02-12 00:09:20 +0300

    Hw3. After review. Server tests

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,7 +2,6 @@ from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 
 from django.shortcuts import render, get_object_or_404, redirect
-from django.db.models import Count
 
 from .models import Post, Group, User
 from .forms import PostForm
@@ -36,7 +35,7 @@ def group_posts(request, slug):
     post_list = group.posts.select_related('author')
 
     posts = get_paginator(post_list, request)
-    
+
     context = {
         'group': group,
         'page_obj': posts,
@@ -45,9 +44,9 @@ def group_posts(request, slug):
 
 
 def profile(request, username):
-    user = get_object_or_404(User, username=username) 
+    user = get_object_or_404(User, username=username)
     post_list = user.posts.all()
-    
+
     posts = get_paginator(post_list, request)
 
     context = {
@@ -61,7 +60,7 @@ def profile(request, username):
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     author = post.author
-    post_count_user= author.posts.count()
+    post_count_user = author.posts.count()
     context = {
         'post': post,
         'post_count_user': post_count_user
